13f20ed77b74740e4ad46684a2064e54608df56f
MutableTuple <mutabletuple39@gmail.com>
2025-01-02 10:30:59 +0530

    added a loader

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./globals.css"; // Make sure to import the CSS file for the animation
 
-export default function loading() {
+export default function Loading() {
   return (
     <div className="h-screen flex items-center justify-center">
-      page is loading....
+      <div className="loading-container">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
     </div>
   );
 }
