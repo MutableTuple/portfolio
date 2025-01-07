8fc5d1e7fd9a0bc7113b7d0532b5b977e83c9bc0
MutableTuple <mutabletuple39@gmail.com>
2025-01-07 10:37:57 +0530

    fixes

--- app/_components/Mainbar.jsx
+++ app/_components/Mainbar.jsx
@@ -7,7 +7,7 @@ export default async function Mainbar() {
   const projects = await getAllProjects();
   return (
     <div className="h-screen overflow-scroll overflow-x-hidden p-4">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-4">
         {projects.map((project) => (
           <Box
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Mainbar from "./_components/Mainbar";
 import Navbar from "./_components/Navbar";
 import Sidebar from "./_components/Sidebar";
 export default function Home() {
+  const revalidate = 30;
   return (
     <div className="">
       <Mainbar />
